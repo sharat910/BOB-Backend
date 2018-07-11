7c29eb620ae36eba5e5a29fb58af6dfe148c42ac
Sharat Chandra <sharat910@gmail.com>
2018-07-11 13:44:31 +0530

    Added initial data loading functionality with django fixtures

--- reset.sh
+++ reset.sh
@@ -1,7 +1,9 @@
 rm db.sqlite3
 rm -rf main/migrations/__pycache__
-rm main/migrations/000*.py
+rm main/migrations/0001*.py
+mv main/migrations/0002* main/migrations/backup/
 python manage.py makemigrations
+mv main/migrations/backup/0002* main/migrations
 python manage.py migrate
 #python manage.py createsuperuser
 #python manage.py runserver
