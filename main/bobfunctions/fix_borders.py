c839278bfe07eda321e3a15fbdf77ef6da83833a
Sharat Chandra <sharat910@gmail.com>
2018-07-11 13:42:52 +0530

    Added monthly statement generator.

---
+++ main/bobfunctions/fix_borders.py
@@ -0,0 +1,38 @@
+from itertools import product
+import types
+import openpyxl
+from openpyxl import worksheet
+from openpyxl.utils import range_boundaries
+
+
+def patch_worksheet():
+    """This monkeypatches Worksheet.merge_cells to remove cell deletion bug
+    https://bitbucket.org/openpyxl/openpyxl/issues/365/styling-merged-cells-isnt-working
+    Thank you to Sergey Pikhovkin for the fix
+    """
+
+    def merge_cells(self, range_string=None, start_row=None, start_column=None, end_row=None, end_column=None):
+        """ Set merge on a cell range.  Range is a cell range (e.g. A1:E1)
+        This is monkeypatched to remove cell deletion bug
+        https://bitbucket.org/openpyxl/openpyxl/issues/365/styling-merged-cells-isnt-working
+        """
+        if not range_string and not all((start_row, start_column, end_row, end_column)):
+            msg = "You have to provide a value either for 'coordinate' or for\
+            'start_row', 'start_column', 'end_row' *and* 'end_column'"
+            raise ValueError(msg)
+        elif not range_string:
+            range_string = '%s%s:%s%s' % (get_column_letter(start_column),
+                                          start_row,
+                                          get_column_letter(end_column),
+                                          end_row)
+        elif ":" not in range_string:
+            if COORD_RE.match(range_string):
+                return  # Single cell, do nothing
+            raise ValueError("Range must be a cell range (e.g. A1:E1)")
+        else:
+            range_string = range_string.replace('$', '')
+
+        if range_string not in self._merged_cells:
+            self._merged_cells.append(range_string)
+
+    worksheet.Worksheet.merge_cells = merge_cells
---
+++ main/bobfunctions/monthly_statements.py
@@ -0,0 +1,171 @@
+from openpyxl import load_workbook
+from pprint import pprint
+from datetime import date,datetime
+import requests
+import json
+import xlrd
+import calendar
+from fix_borders import patch_worksheet
+
+
+TEMPLATE_ROOT = 'form_templates'
+TEMPLATE_DIR = 'Monthly'
+
+def fetch_batch():
+    r = requests.get('http://localhost:8000/api/batch/2')
+    batch_dict = json.loads(r.text)
+    with open('batch.json', 'w') as outfile:
+     json.dump(batch_dict, outfile, sort_keys = True, indent = 4,
+               ensure_ascii = False)
+    return batch_dict
+
+def fetch_royaltyrate():
+    r = requests.get('http://localhost:8000/api/royaltyrate/get_rate')
+    rate = json.loads(r.text)['month_royalty']
+    return rate
+
+
+class MonthlyStatement(object):
+    """docstring for MonthlyStatement."""
+    def __init__(self, batch, month):
+        super(MonthlyStatement, self).__init__()
+        self.batch = batch
+        self.month = month
+        self.wb = None
+        self.ws = None
+        self.generate_monthly_statement()
+
+    def generate_monthly_statement(self):
+        self.set_wb_ws()
+        self.fill_top()
+        self.fill_students()
+        self.wb.save(self.get_final_file_path())
+
+    def get_template_file_path(self,type,file_type='xlsx'):
+        return "%s/%s/FormB_%s.%s" % (TEMPLATE_ROOT,TEMPLATE_DIR,type,file_type)
+
+    def get_final_file_path(self,file_type='xlsx'):
+        return '../../media/monthly_statements/%s_%s_%s.%s' \
+        % (self.batch['teacher_name'],self.batch['level_detail'],\
+        calendar.month_abbr[self.month],file_type)
+
+    def bob_or_little_bob(self,level):
+        if level <= 10:
+            return 'bob'
+        else:
+            return 'little_bob'
+
+    def get_excel_workbook(self,level):
+        type = self.bob_or_little_bob(level)
+        path = self.get_template_file_path(type)
+        patch_worksheet()
+        return load_workbook(path)
+
+    def set_wb_ws(self):
+        self.wb = self.get_excel_workbook(self.batch['level'])
+        self.ws = self.wb.active
+
+    def get_month(self,date_str,string=False):
+        dt = datetime.strptime(date_str,"%Y-%m-%d")
+        if string:
+            return dt.strftime("%b")
+        return dt.month
+
+    def fill_top(self):
+        today = date.today().strftime("%m/%d/%Y")
+        self.ws['S5'] = today
+        centre_name = str(self.batch['centre_exp']['name']).upper()
+        centre_area = str(self.batch['centre_exp']['area']).upper()
+        self.ws['J6'] = centre_name
+        self.ws['Q6'] = centre_area
+        self.ws['I7'] = self.batch['id']
+        self.ws['N7'] = self.batch['level_detail']
+        self.ws['P7'] = self.batch['day']
+        self.ws['T7'] = self.batch['timing']
+        self.ws['I8'] = str(self.batch['centre_exp']['code'])
+        self.ws['M8'] = self.batch['teacher_name']
+        self.ws['S8'] = datetime.strptime(self.batch['level_start_date'],"%Y-%m-%d").strftime("%m/%d/%Y")
+
+    def fill_summary(self,total_data):
+        royalty = fetch_royaltyrate()
+        self.ws['L30'] = royalty
+        self.ws['L31'] = royalty
+        self.ws['L32'] = royalty
+
+    def fetch_current_level_feerecords(self,student):
+        return filter(lambda feerecord: feerecord['level'] == self.batch['level'],\
+                    student['feerecords'])
+
+    def get_student_row_data(self,batch_running_months,student):
+        data = {
+        'code': student['code'],
+        'name': student['name']
+        }
+
+        feerecords = self.fetch_current_level_feerecords(student)
+        if student['dropped']:
+            if self.get_month(student['date_dropped']) == self.month:
+                # Student dropped this month
+                data['dropped'] = "DROPPED"
+            else:
+                return None
+        else:
+            data['dropped'] = ""
+
+        payment_row = ['NP','NP','NP']
+        for feerecord in feerecords:
+            if feerecord['fee_type'] == 'Level':
+                month_of_payment = self.get_month(feerecord['date_of_payment'])
+                if month_of_payment == self.month:
+                    payment_row = (3*[feerecord['fee_receipt_no']])
+                elif month_of_payment < self.month:
+                    payment_row = (3*['PAID'])
+                else:
+                    payment_row = (3*['NP'])
+            elif feerecord['fee_type'] == 'Month':
+                month_of_payment = self.get_month(feerecord['date_of_payment'])
+                month_paid_for = feerecord['months'][0]
+                assert month_paid_for in batch_running_months
+                idx = batch_running_months.index(month_paid_for)
+                if month_of_payment == self.month:
+                    payment_row[idx] = feerecord['fee_receipt_no']
+                elif month_of_payment < self.month:
+                    payment_row[idx] = 'PAID'
+                else:
+                    payment_row[idx] = 'NP'
+        assert len(payment_row) == 3
+        data['payment'] = payment_row
+        return data
+
+    def get_running_months(self,batch_start_date):
+        batch_start_month = self.get_month(batch_start_date)
+        batch_running_months = range(batch_start_month,batch_start_month+3)
+        return batch_running_months
+
+    def fill_students(self):
+        batch_running_months = self.get_running_months(self.batch['level_start_date'])
+        students = sorted(self.batch['students'],key= lambda x: x['code'])
+        row_num = 13
+        total_data = []
+        for student in students:
+            row_data = self.get_student_row_data(batch_running_months,student)
+            print(row_data)
+            if row_data:
+                self.fill_row(row_data,row_num)
+                total_data.append(row_data)
+                row_num+=1
+            else:
+                pprint(student)
+        self.fill_summary(total_data)
+
+    def fill_row(self,row_data,row_num):
+        self.ws['G%d' % row_num] = row_data['code']
+        self.ws['I%d' % row_num] = row_data['name']
+        self.ws['O%d' % row_num] = row_data['dropped']
+        self.ws['Q%d' % row_num] = row_data['payment'][0]
+        self.ws['R%d' % row_num] = row_data['payment'][1]
+        self.ws['T%d' % row_num] = row_data['payment'][2]
+
+if __name__ == '__main__':
+    batch = fetch_batch()
+    m = MonthlyStatement(batch,7)
