c10496d977b790ebd5b780fe001404e8006b38fc
Sharat Chandra <sharat910@gmail.com>
2018-07-18 18:33:56 +0530

    Dockerify django app.

--- bobmanager/settings.py
+++ bobmanager/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
